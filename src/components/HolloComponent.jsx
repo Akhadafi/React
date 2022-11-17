20d2fbdfda55f248e93af0cc639f65bc263d698f
Akhadafi <khadafi.dev@gmail.com>
2022-11-17 21:51:23 +0700

    StateLess Component & StateFullComponent

---
+++ src/components/HolloComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './HolloComponent.css'
+
+const HolloComponent = () => {
+  return (
+    <div className='text_p'>HolloComponent</div>
+  )
+}
+
+export default HolloComponent
---
+++ src/container/StateFullComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StateFullComponent = () => {
+  return (
+    <div>StateFullComponent</div>
+  )
+}
+
+export default StateFullComponent
